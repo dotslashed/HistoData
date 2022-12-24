810c96828e7a59d5834fe5900e00b83b1204186a
Bhargab Kaushik <73475291+dotslashed@users.noreply.github.com>
2022-12-25 00:37:15 +0530

    Update histodata.py

--- histodata.py
+++ histodata.py
@@ -12,18 +12,19 @@ try:
 
 	resp = requests.get(url=url, headers = headers)
 
-
 	ip_list = []
 	new_l = resp.json()['records']
 
 	for item in range(len(new_l)):
 		new_l2 = new_l[item]['values']
 		for stuff in range(len(new_l2)):
-			if new_l2[stuff]['ip'] not in ip_list:
-				ip_list.append(new_l2[stuff]['ip'])
+			new_l2[stuff]['org'] = new_l[item]['organizations']
+			freak = new_l2[stuff]['ip'] + " " + new_l2[stuff]['org'][0]
+			if freak not in ip_list:
+				ip_list.append(freak)
 
 	for i in range(len(ip_list)):
 		print(ip_list[i])
 		
 except IndexError:
-	print('Usage: python3 histodata.py <domain> <securitytrails API key>')
+	print('Usage: python3 historyData.py <domain> <securitytrails API key>')
